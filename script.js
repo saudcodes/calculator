52fc8a29976f8d20a02f250235f58c8375ba7721
saudcodes <90344904+saudcodes@users.noreply.github.com>
2022-01-26 17:26:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+function subtract(num1, num2) {
+    return num1 - num2
+}
+function multiply(num1, num2) {
+    return num1 * num2
+}
+function divide(num1, num2) {
+    return num1 / num2
+}
 
-console.table("hii")
+function operand(operator, arg1, arg2) {
+    if (operator == "+") {
+        return add(arg1, arg2)
+    }
+    else if(operator == "-") {
+        return subtract(arg1, arg2)
+    }
+}
+
+console.log(operand("-", 5, 6))
