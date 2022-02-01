df601999375d17f7c20568007388787c82bf2e41
Saud Aijaz <90344904+saudcodes@users.noreply.github.com>
2022-02-01 07:23:08 +0000

    update code

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-let playerNum1 = 5
-let playerNum2 = 5
-let playerInput = "+"
-
-function add(num1, num2) {
-    return num1 + num2
-}
-function subtract(num1, num2) {
-    return num1 - num2
-}
-function multiply(num1, num2) {
-    return num1 * num2
-}
-function divide(num1, num2) {
-    return num1 / num2
-}
 
 
 
-// function secondnum() {
-//     if (playerNum1 != "") {
-//         return playerNum2
-//     }
-//     else {
-//         return console.log("please fill 1st one")
-//     }
-// }
 
+
+
+let playerNum1 = "10";
+ let playerNum2 = "3";
+let playerInput = "*"
+let number = document.querySelector("#number")
+let buttons = document.querySelectorAll("button")
+
+
+
+
+
+
+buttons.forEach( (button) => {
+    button.addEventListener("click", () => {
+    
+        number.textContent = button.innerText
+    }
+    )
+})
 
 
 
@@ -34,28 +30,23 @@ function operand(operator, arg1, arg2) {
     arg2 = playerNum2
     operator = playerInput
     if (operator == "+") {
-        return add(arg1, arg2)
+        return arg1 + arg2
+        
     }
  else if (operator == "-") {
-    return subtract(arg1, arg2)
+    return arg1 - arg2
 } 
 else if (operator == "*") {
-    return multiply(arg1, arg2)
+    return arg1 * arg2
 }
 else if (operator == "/") {
-    return divide(arg1, arg2)
-}
-console.log("it worked")
+    return arg1 / arg2
 }
 
 
+}
+let text = document.querySelector("#text")
+text.textContent = operand();
 
+console.log(operand())
 
-// function result() {
-//     if (playerNum1 != "" && playerNum2 != "" && playerInput != "") {
-//         return operand()
-//     }
-//     else {
-//     return console.log("please fill all first")
-//     }
-// }
